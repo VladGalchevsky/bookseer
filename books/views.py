3f9c4f22f643ce908f50a8550c05644381ed18fa
VladGalchevsky <vladgalchevsky97@gmail.com>
2023-01-02 21:50:27 +0200

    page navigation

--- books/views.py
+++ books/views.py
@@ -14,6 +14,20 @@ def books_list(request):
         books = books.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             books = books.reverse()
+            
+    # paginate books
+    paginator = Paginator(books, 5)
+    page = request.GET.get('page')
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+    # If page is not an integer, deliver first page.
+        books = paginator.page(1)
+    except EmptyPage:
+    # If page is out of range (e.g. 9999), deliver
+    # last page of results.
+        books = paginator.page(paginator.num_pages)
+    
     return render(request, 'books/books_list.html', {'books': books})
 
 
